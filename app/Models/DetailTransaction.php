4333a3504b454ad63e69bb4bfff796d7f34abc7a
Setiawan Dimas <arimurti95@gmail.com>
2024-02-08 21:33:19 +0700

    check point

--- app/Models/DetailTransaction.php
+++ app/Models/DetailTransaction.php
@@ -22,6 +22,6 @@ class DetailTransaction extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class,'id_product','id');
     }
 }
